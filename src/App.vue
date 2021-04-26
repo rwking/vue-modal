b6d030a19652cf19b44bbcd9f73c147e397d9a6d
Rich King <rwking@gmail.com>
2021-04-25 21:55:19 -0400

    first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br />
+  <!-- <p><input type="text" ref="name"></p> -->
+  <p><button @click="toggleModal">Toggle modal</button></p>
+  <transition name="slide" :duration="500">
+      <Modal v-if="showModal" @toggleModal="toggleModal" />
+  </transition>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "Demonstration of modal pop-out",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      console.log(this.$refs.name);
+      //   this.$refs.name.classList.remove('input-gray')
+      //   this.$refs.name.focus()
+      this.showModal = !this.showModal
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +41,37 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #dddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+.input-gray {
+  background-color: rgb(199, 199, 199);
+}
+.slide-enter, .slide-leave-active {
+  right: -100%;
+  transition: all .8s;
+}
+
+
+/* General styles unrelated to slide in */
+body {
+  font-family: "Mulish", sans-serif;
+}
+
+.toggle {
+  margin: 1em;
+}
+
+button {
+  padding: 0.5em 1em;
+  border-radius: 3em;
+  font-size: 1.1em;
+}
+
+h1 {
+  font-weight: 200;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="backdrop">
+    <div class="modal" id="modal">
+    <button class="close-btn" @click="$emit('toggleModal')">X</button>
+      <p class="modal-text">Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["toggleModal"],
+};
+</script>
+
+<style>
+.modal {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  margin-left: 10%;
+  margin-top: 10px;
+  background: rgba(101, 103, 131, 0.85);
+  border-radius: 0px;
+  border-left: 2px solid rgba(65, 69, 109, 0.85);
+  border-top: 2px solid rgba(65, 69, 109, 0.85);
+  transition: all 0.5s ease-in-out;
+}
+.modal-text {
+  color: white;
+  font-weight: bold;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(255, 255, 255, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.close-btn {
+  border: none;
+  font-weight: bold;
+  font-size: 1.5em;
+  margin-left: 10%;
+  background: transparent;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 0.5em;
+}
+</style>
